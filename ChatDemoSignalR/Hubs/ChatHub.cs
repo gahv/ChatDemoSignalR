ccc5a5457483ec08619fdbdab618db23626c2706
rodolfofadino <rodolfo.fadino@gmail.com>
2013-01-09 01:37:52 -0200

    First commit

---
+++ ChatDemoSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatDemoSignalR.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public void EnviarMensagem(string nome, string mensagem)
+        {
+            Clients.All.publicarMensagem(nome, mensagem);
+        }
+    }
+}
---
+++ ChatDemoSignalR/Scripts/Chat.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="jquery-1.7.1.min.js" />
+/// <reference path="jquery.signalR-1.0.0-rc1.js" />
+
+$(function () {
+    var chat = $.connection.chatHub;
+
+    chat.client.publicarMensagem = function (nome, mensagem) {
+        var containerNome = $('<span/>').text(nome).html();
+        var containerMensagem = $('<div/>').text(mensagem).html();
+
+        $("#conversa").append(
+            '<li><strong>'
+                + containerNome +
+                '</strong>: '
+                + containerMensagem + '</li>');
+    };
+    
+    $.connection.hub.start().done(function () {
+        $("#enviar").click(function () {
+            var nome = $("#nome").val();
+            var mensagem = $("#mensagem").val();
+
+            chat.server.enviarMensagem(nome, mensagem);
+
+            $("#mensagem").val('');
+        });
+    });
+
+});
